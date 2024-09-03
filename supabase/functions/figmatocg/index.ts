5822ab24c63158bc203b0d89692b52ce43dffcbf
nishat-nasir <nishatnasir00@gmail.com>
2024-09-03 18:02:22 +0900

    Updatded supabase edge functino

--- supabase/functions/figmatocg/index.ts
+++ supabase/functions/figmatocg/index.ts
@@ -10,12 +10,14 @@ console.log("Hello from Functions!");
 const supabase = createClient(
   Deno.env.get("SUPABASE_URL")!,
   Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!,
+  // "https://ofyngtjccmbxofnztary.supabase.co",
+  // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9meW5ndGpjY21ieG9mbnp0YXJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI0MzcyMzYsImV4cCI6MjAxODAxMzIzNn0.c3oJ3CENN5hWSpvcB5ev7BJBC2SsqhPB4OTFU39RqYM",
 );
 
 const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers":
-    "authorization, x-client-info, apikey, content-type",
+  "Access-Control-Allow-Origin": "*", // Allows all origins; change to specific domain in production
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
 Deno.serve(async (req) => {
@@ -23,11 +25,66 @@ Deno.serve(async (req) => {
     return new Response("ok", { headers: corsHeaders });
   }
 
-  const { id, pageIndex, jsonData } = JSON.parse(await req.text());
+  const { id, pageIndex, jsonData, page } = JSON.parse(await req.text());
 
-  const { data: projectData, error } = await supabase.from("projects").select(
-    "routes",
-  ).eq("id", id).single();
+  console.log(id);
+  console.log(jsonData);
+  console.log(pageIndex);
+  console.log(page["id"]);
+
+  //  TODO: Get the jsonData, map it to ComponentV2 and add it to the components table in supabase
+
+  for (let i = 0; i < jsonData.length; i++) {
+    const item = jsonData[i];
+    console.log("Inserting item:", item);
+
+    // Ensure width and height are fetched from the correct place
+    const width = item.options.width ? parseInt(item.options.width) : null;
+    const height = item.options.height ? parseInt(item.options.height) : null;
+    console.log(item);
+    const { data: componentData, error } = await supabase.from("components")
+      .insert([
+        {
+          name: item.name,
+          type: item.type,
+          dx: item.dx,
+          dy: item.dy,
+          width: width,
+          height: height,
+          options: item.options,
+          page_id: page["id"],
+          version: "1.0",
+          is_lib_comp: false,
+          index_in_page: i,
+        },
+      ]);
+
+    if (error) {
+      console.log(error);
+      return new Response(
+        JSON.stringify({ error: "Error fetching component data" }),
+        { status: 500 },
+      );
+    }
+
+    console.log("componentData", componentData);
+
+    if (componentData) {
+      console.log("Component added to components table");
+    } else {
+      console.error("Error adding component to components table");
+    }
+  }
+
+  const { data: projectData, error } = await supabase.from("pages").select("*")
+    .eq(
+      "id",
+      id,
+    ).single();
+
+  console.log("SUPABSE URL::: ", Deno.env.get("SUPABASE_URL"));
+
+  console.log("projectData", projectData);
 
   if (error) {
     console.log(error);
@@ -37,9 +94,9 @@ Deno.serve(async (req) => {
     );
   }
 
-  const routes = projectData.routes || [];
+  const pages = projectData.pages || [];
 
-  if (routes.length > 0 && routes[0].cgCustomComponents) {
+  if (pages.length > 0 && pages[0].cgCustomComponents) {
     jsonData.forEach((item: any) => {
       const newItem = {
         id: null,
@@ -56,16 +113,16 @@ Deno.serve(async (req) => {
         isPublic: false,
       };
 
-      routes[pageIndex].cgCustomComponents.push(newItem);
+      pages[pageIndex].cgCustomComponents.push(newItem);
     });
   } else {
     console.error(
-      "Routes array is empty or does not have cgCustomComponents",
+      "pages array is empty or does not have cgCustomComponents",
     );
   }
 
   const { data: updatedData } = await supabase.from("projects")
-    .update({ routes }).eq("id", id);
+    .update({ pages }).eq("id", id);
 
   if (error) {
     console.log(error);
@@ -76,7 +133,7 @@ Deno.serve(async (req) => {
   }
 
   const response = {
-    message: `Added jsonData to routes for project with id ${id}`,
+    message: `Added jsonData to pages for project with id ${id}`,
     updatedData,
   };
 
